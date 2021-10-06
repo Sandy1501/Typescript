012c2085bdeffab5074d8fe827fa417c362e4fcb
Sandy1501 <81281765+Sandy1501@users.noreply.github.com>
2021-10-06 16:45:58 -0500

    version 1

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var color = "blue";
+var decimal = 6;
+var variableboo = false;
+var nulo = null;
+var indefinido = undefined;
---
+++ scripts.ts
@@ -0,0 +1,5 @@
+let color: string = "blue";
+let decimal: number = 6;
+let variableboo: boolean = false;
+let nulo: null = null;
+let indefinido: undefined = undefined;
